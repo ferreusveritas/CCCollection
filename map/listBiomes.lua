06ce23be6f8b397bfa6be4cdbf7b06c93708b5a4
ferreusveritas <ferreusveritas@gmail.com>
2018-09-29 20:21:59 -0400

    update palette creator

--- map/listBiomes.lua
+++ map/listBiomes.lua
@@ -5,9 +5,11 @@ file = io.open("biomeList.csv", "a");
 
 for id = 0, 255 do
 	local name = terr.getBiomeName(id);
+    print(id, name);
 	if name then
 		file:write(id .. "," .. name, "\n");
 	end
+    sleep(0.05);
 end
 
 file:close();
